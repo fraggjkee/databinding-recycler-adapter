e173919d711049b2d8b04813a82910bfed5da8a1
Anton <Anton.Stulnev@macys.com>
2020-05-23 22:38:44 +0300

    Added jitpack.io repo + simplified sample app

--- sample/src/main/java/com/fraggjkee/databindingadapter/MainViewModel.kt
+++ sample/src/main/java/com/fraggjkee/databindingadapter/MainViewModel.kt
@@ -1,6 +1,5 @@
 package com.fraggjkee.databindingadapter
 
-import android.os.Handler
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -13,10 +12,6 @@ class MainViewModel : ViewModel() {
 
     init {
         _recyclerItems.value = loadUsers().map { it.toRecyclerItem() }
-
-        Handler().postDelayed({
-            _recyclerItems.value = loadUsers2().map { it.toRecyclerItem() }
-        }, 3000)
     }
 
     private fun loadUsers(): List<User> =
@@ -26,16 +21,6 @@ class MainViewModel : ViewModel() {
             User(3, "Johannes", "Brahms"),
             User(4, "Franz", "Schubert"),
             User(5, "Giuseppe", "Verdi")
-        )
-
-    private fun loadUsers2(): List<User> =
-        listOf(
-            User(1, "Pyotr", "Tchaikovsky"),
-            User(2, "Richard", "Wagner"),
-            User(3, "Johannes", "Brahms"),
-            User(5, "Giuseppe", "Verdi"),
-            User(4, "Franz", "Schubert"),
-            User(6, "Justin", "Bieber")
         )
 }
 
